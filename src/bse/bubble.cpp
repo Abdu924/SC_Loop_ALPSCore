0e9dc97133c29bdbc4669f3ea67ac032f67ac61b
DOMINIQUE GEFFROY <geffroy.dominique@free.fr>
2018-06-03 11:52:26 +0200

    debug

--- src/bse/bubble.cpp
+++ src/bse/bubble.cpp
@@ -77,6 +77,7 @@ Bubble::Bubble(alps::hdf5::archive &h5_archive,
                                            [n_legendre][n_legendre][N_boson][nb_q_points_per_proc * world_size]);
      std::fill(world_lattice_legendre_values_.origin(),
                world_lattice_legendre_values_.origin() + world_lattice_legendre_values_.num_elements(), 0.0);
+     MPI_Barrier(MPI_COMM_WORLD);     
 }
 
 std::vector<Eigen::MatrixXcd> Bubble::get_greens_function(Eigen::Ref<Eigen::VectorXd> k_point,
