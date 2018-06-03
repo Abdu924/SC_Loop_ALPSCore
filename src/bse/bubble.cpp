f31b2527f60dc55604ab87ec1fd53e1b9141547b
DOMINIQUE GEFFROY <geffroy.dominique@free.fr>
2018-06-03 19:11:22 +0200

    1line

--- src/bse/bubble.cpp
+++ src/bse/bubble.cpp
@@ -101,7 +101,6 @@ std::vector<Eigen::MatrixXcd> Bubble::get_greens_function(Eigen::Ref<Eigen::Vect
      return output;
 }
 
-
 void Bubble::dump_bubble_hdf5(const alps::params& parms) {
      if (world_rank_ == 0) {
           int line_idx, col_idx;
